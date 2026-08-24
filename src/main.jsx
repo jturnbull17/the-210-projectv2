be5fb4bfd87fdf598adb34983adc53cdf9542af9
Turnbull <jack.turnbull@capgemini.com>
2026-08-24 14:33:11 +0100

    Accessibility fixes and read more link

--- src/main.jsx
+++ src/main.jsx
@@ -1061,12 +1061,6 @@ function CountryPage({id}){const[data]=useData();if(!data)return <Loading/>;cons
 <div>
 <span>{l.date_text}</span><h3>{l.name}</h3>
 <p>{l.summary}</p>
-
-
-<span className="read-more-link">
-        Read the full story →
-      </span>
-
 </div>
 </a>
 )}
