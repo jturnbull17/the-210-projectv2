87b7258b10590274b83e8a47ba2b825f9843687e
Turnbull <jack.turnbull@capgemini.com>
2026-08-24 14:25:23 +0100

    Accessibility fixes and read more link

--- src/main.jsx
+++ src/main.jsx
@@ -1059,8 +1059,8 @@ function CountryPage({id}){const[data]=useData();if(!data)return <Loading/>;cons
   {locs.map(l => (
     <a
       key={l.id}
-      href={`/archive/${country.id  src={l.hero_image}
-        alt={late_text}</span>
+      href={`/archive/${country.id}/${l.slug}<div>
+        <span>{l.date_text}</span>
 
         <h3>{l.name}</h3>
 
